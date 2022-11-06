ee03132ce48bb6e726da68f838c3339d9ed52dd6
himanshi4902 <sharmahimanshi0094@gmail.com>
2022-11-06 17:41:14 +0530

    Co-authored-by: Anurag Verma <Anurag-error404@users.noreply.github.com> Co-authored-by: ishitarakchhit <ishitarakchhit@users.noreply.github.com>

--- user/src/pages/landingpage/Content.jsx
+++ user/src/pages/landingpage/Content.jsx
@@ -3,16 +3,16 @@ import Rocket from '../../../src/assets/Rocket.png';
 
 function Content() {
     return (
-        <div class="landingpage">
+        <div>
                 <div className='landingtext'>
                     <h1>WELCOME TO</h1>
                     <h1>SLOT BOOKING</h1>
                     <h1>PORTAL OF IGDTUW</h1>
                      <h2 className='landinglogin'>Login as:</h2>
                 </div>
-                <div className="Buttons">
-                    <button className='Admin'>Admin</button>
-                    <button className='Student'>Student</button>
+                <div className="landingButtons">
+                    <button className='landingAdmin'>Admin</button>
+                    <button className='landingStudent'>Student</button>
                 </div>
                 <figure>
                     <img src={Rocket} alt="Rocket"  className="landingRocket" />
