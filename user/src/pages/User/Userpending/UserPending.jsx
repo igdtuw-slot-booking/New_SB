bedd69d43495176da1b4df35d586cb046511eb29
ishita067btmae21@igdtuw.ac.in <104856237+ishitarakchhit@users.noreply.github.com>
2023-03-07 01:00:44 +0530

    modification of admin and user pages in frontend

--- user/src/pages/User/Userpending/UserPending.jsx
+++ user/src/pages/User/Userpending/UserPending.jsx
@@ -9,11 +9,14 @@ import {Link} from "react-router-dom";
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns } from '../../../datatablesource.js';
 
-export default function UserPending() {
 
-    //const {data,loading,error} = useFetch("/event?status=Pending");
-    // const handleClick = () => 
-    const data=[5,8,2,6];
+export default function UserPending() {
+  const data = [
+    { _id: '1', name: 'Event 1', status: 'Pending' },
+    { _id: '2', name: 'Event 2', status: 'Pending' },
+    { _id: '3', name: 'Event 3', status: 'Pending' },
+    { _id: '4', name: 'Event 4', status: 'Pending' },
+  ];
 const loading = false;
   return (
     <div>
@@ -47,7 +50,7 @@ const loading = false;
                 "Loading"
             ) : (
                 <>
-                    <DataGrid
+                    <DataGrid                 
                     id={data[0]}
                     rows={data}
                     columns={userColumns}
@@ -67,3 +70,61 @@ const loading = false;
     </div>
   )
 }
+// export default function UserPending() {
+
+//     //const {data,loading,error} = useFetch("/event?status=Pending");
+//     // const handleClick = () => 
+//     const data=[5,8,2,6];
+// const loading = false;
+//   return (
+//     <div>
+//         <Navbar/>
+//         <Sidebar/>
+
+// { 
+//         <div style={{ height: 400, width: '100%' }} className="Table-user">
+//       <div style={{ display: 'flex', height: '100%' }}>
+//         <div style={{ flexGrow: 1 }}>
+//             <div className='listTitle'>Events</div>
+
+//                 <div className='in-nav'>
+//                     <ul>
+//                     <li>
+//                           <Link to="/" style={{color:'inherit', textDecoration:"none"}}><h6>Pending</h6></Link>
+//                         </li>
+//                         <li>
+//                           <Link to="approved" style={{color:'inherit', textDecoration:"none"}}><h6>Approved</h6></Link>
+//                         </li>
+//                         <li>
+//                           <Link to="declined" style={{color:'inherit', textDecoration:"none"}}><h6>Declined</h6></Link>
+//                         </li>
+//                         <li>
+//                           <Link to="cancelled" style={{color:'inherit', textDecoration:"none"}}><h6>Cancelled</h6></Link>
+//                         </li>
+//                     </ul>
+//                 </div>
+
+//             {loading ? (
+//                 "Loading"
+//             ) : (
+//                 <>
+//                     <DataGrid
+//                     id={data[0]}
+//                     rows={data}
+//                     columns={userColumns}
+//                     pageSize={6}
+//                     rowsPerPageOptions={[5]}
+//                     getRowId={row=>row._id}
+//                     />
+//                 </>
+//             )}
+            
+//         </div>
+//       </div>
+//     </div>
+// }
+
+//     <UserDataBox/>
+//     </div>
+//   )
+// }
