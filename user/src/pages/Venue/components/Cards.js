fe2db49944a21ff37b714ec1c9ae0e003efe6ef8
jahnvi15 <91893464+jahnvi15@users.noreply.github.com>
2023-03-06 22:47:58 +0530

    Edited Backend

--- user/src/pages/Venue/components/Cards.js
+++ user/src/pages/Venue/components/Cards.js
@@ -1,10 +1,107 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './modal.css';
+
+const Cards = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [venues, setVenues] = useState([]);
+ 
+  const [selectedVenue, setSelectedVenue] = useState(null);
+
+  useEffect(() => {
+    async function fetchVenues() {
+      const res = await fetch('http://localhost:8877/api/venue/svenues');
+      const data = await res.json();
+      setVenues(data);
+    }
+
+    fetchVenues();
+  }, []);
+
+
+  const handleBookClick = (venue) => {
+    setSelectedVenue(venue);
+    setShowModal(true);
+  };
+
+  return (
+    <>
+      {venues.map((venue) => (
+        <Card key={selectedVenue.id} style={{ width: '19rem', border: 'none' }} className="hover mb-4">
+          <Card.Img variant="top" className="cd" src={selectedVenue.photos} />
+          console.log('data:', data);
+          console.log('photos:', data.photos);
+          <div className="card_body">
+            <div className="info">
+              <div className="upper_data d-flex justify-content-between align-items-center">
+                <span className="text_upper">
+                  <h4 className="mt-2">{selectedVenue.name}</h4>
+                  <h5 className="w-500 address">{selectedVenue.location}</h5>
+                </span>
+
+                <span className="purple">
+                  <Button as="input" type="button" value="Book" variant="book" onClick={() => handleBookClick(venue)} />
+                </span>
+              </div>
+            </div>
+          </div>
+        </Card>
+      ))}
+
+      <Modal show={showModal} onHide={() => setShowModal(false)} className="modal-container-venue">
+        <Modal.Header closeButton className="modal-header-venue">
+          <Modal.Title className="modal-title-venue">Booking Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="modal-body-venue">
+          {selectedVenue.photos && (
+            <Carousel showArrows={true} showThumbs={false} useKeyboardArrows showIndicators={true}>
+              {selectedVenue.photos.map((image, i) => {
+                return (
+                  <div key={i}>
+                    <img src={image} alt="" className="slider-image-venue" />
+                  </div>
+                );
+              })}
+            </Carousel>
+          )}
+          <div className="d-flex justify-content-between align-items-center">
+            <h4 className="mt-2">{selectedVenue.name}</h4>
+            <h5 className="w-500 ">{selectedVenue.location}</h5>
+          </div>
+          <div className="d-flex mt-4">
+            <div className="w-50 text-left">
+              <p>Room No:</p>
+              <p>Location:</p>
+              <p>Capacity:</p>
+              <p>Facilities:</p>
+            </div>
+            <div className="w-50 text-left">
+              <p>{selectedVenue.room_no}</p>
+              <p>{selectedVenue.location}</p>
+              <p>{selectedVenue.capacity}</p>
+            </div>
+          </div>
+          <div className="facilities-container">
+            {selectedVenue.facilities &&
+              selectedVenue.facilities.map((facility) => (
+                <div key={facility} className="facility">
+                {facility}
+                </div>
+                ))}
+                </div>
+                <div className="mt-4 text-center">
+                <Button as="input" type="button" value="Confirm" variant="book" />
+                </div>
+                </Modal.Body>
+                </Modal>
+                </>
+                );
+                };
+/*
 const Cards = ({ data }) => {
   const [showModal, setShowModal] = useState(false);
   const [selectedData, setSelectedData] = useState({});
@@ -17,7 +114,7 @@ const Cards = ({ data }) => {
     }
 };
 
-  return (
+  return ( 
     <>
       {data.map((element, k) => {
         return (
@@ -113,5 +210,5 @@ const Cards = ({ data }) => {
       })}
     </>
   )
-}
+} */
 export default Cards
